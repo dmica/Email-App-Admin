63be6661145cb9627b8e80b7226b924d6f364ad0
Maja Dmitrovic <dmitrovic.maja@gmail.com>
2018-02-22 11:54:41 +0000

    Add files via upload

---
+++ emailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class emailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Maya","Dmitrovic");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
